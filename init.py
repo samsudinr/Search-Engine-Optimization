aaaf0bc201ecdb6ba8ec81532547ec76a8083b67
samsudinr <samsudiney@gmail.com>
2021-04-19 16:57:05 +0700

    Update init.py

--- init.py
+++ init.py
@@ -14,7 +14,7 @@ url_dekstop = "https://www.toyota.astra.co.id/sitemap.xml"
 headers_dekstop = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 headers_mobile = {'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'}
 
-ID_for_upload_File = "1E_znter6dGuMgCGhimcoeLUBoBx1NTwmNjTZCakfRzw"
+ID_for_upload_File = "ID_google_Spreadsheet"
 
 cred_emcanalyticsteam = {
     'pathClientSecret': 'cred/client_secret_emcanalyticsteam.json',
